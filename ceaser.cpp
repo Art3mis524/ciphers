b649ceacb4b84399e4ebb74e1b4f24cd3c63098b
Art3mis524 <joshua20041985@gmail.com>
2024-08-01 10:49:42 +0100

    finished ceaser

--- ceaser.cpp
+++ ceaser.cpp
@@ -2,6 +2,8 @@
 #include <string>
 
 const int SHIFT = 3;
+const int MAX = 126;
+const int MIN = 36;
 
 std::string getUserInput()
 {
@@ -13,27 +15,45 @@ std::string getUserInput()
 
 char getAsciiValue(char character)
 {
-    int ascii = (int) character;
+    int ascii = character;
     std::cout << ascii << std::endl;
     return ascii;
 }
 
 std::string encrypt(std::string str)
 {
+    std::string encryptedString;
     for (int i = 0; i < str.size(); i ++)
     {
-        char currentCharacter = str[i];
-        if (getAsciiValue(currentCharacter) == 32)
+        char character;
+        int asciiValue = getAsciiValue(str[i]);
+        if (asciiValue != 32 && asciiValue > MIN) //32 is a space
         {
-            std::cout << "space found" << std::endl;
+            asciiValue = asciiValue - SHIFT;
+            character = asciiValue;
+            encryptedString.insert(i, 1, character);
+        }
+        
+        else if (asciiValue != 32 && asciiValue < MIN)
+        {
+            asciiValue = asciiValue + 94;
+            asciiValue = asciiValue - SHIFT;
+            character = asciiValue;
+            encryptedString.insert(i, 1, character);
+        }
+        else
+        {
+            character = asciiValue;
+            encryptedString.insert(i, 1, character);
         }
     }
+    return encryptedString;
 }
 
 int main()
 {
     std::string name;
     std::string strToEncrypt = getUserInput();
-    
+    std::cout << encrypt(strToEncrypt) << std::endl;
     return 0;
 }
