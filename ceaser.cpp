f0fb8fd70e35e92bec91df461d287a9310904b91
Art3mis524 <joshua20041985@gmail.com>
2024-08-01 08:52:22 +0100

    ceaser get input

--- ceaser.cpp
+++ ceaser.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
+const int SHIFT = 3;
+
+std::string getUserInput()
+{
+    std::string str;
+    std::cout << "enter string" << std::endl;
+    std::getline(std::cin, str);
+    return str;
+}
+
+std::string encrypt(std::string)
+{
+    
+}
+
 int main()
 {
-    std::cout << "hello world" << std::endl;
-
+    std::string name;
+    std::string strToEncrypt = getUserInput();
+    
     return 0;
 }
